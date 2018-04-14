4648d4e867aef9eef47126ea165c41733d08457d
Envision Cheng <31516802+envisioncheng@users.noreply.github.com>
2018-04-14 19:20:29 +0800

    first commit

---
+++ swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 28;
+    int y = 50;
+
+    printf("x is %i\n", x);
+    printf("y is %i\n", y);
+
+    // sqapping...
+    print("x is now %i\n", x);
+    print("y is now %i\n", y)
+}
